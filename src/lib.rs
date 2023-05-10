5bdecd63311b0639aec7d9f9cf2f3d09f0a9c0e6
JayCeM <johann.carl@online.de>
2023-05-10 11:08:37 +0200

    add 1-tree lower bound computation to the program

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use crate::parser::TravellingSalesmanProblemInstance;
-use std::{fs::File, io::Read, path::PathBuf};
+use std::{error::Error, fs::File, io::Read, path::PathBuf};
 
 use clap::Parser;
+use one_tree::one_tree_lower_bound;
 
 pub mod mst;
 pub mod one_tree;
@@ -15,10 +16,12 @@ pub struct Args {
 }
 
 /// This function calls the main logic of our program.
-pub fn run(args: Args) -> std::io::Result<()> {
+pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
     let mut file = File::open(args.input_file)?;
     let mut xml = String::new();
     file.read_to_string(&mut xml)?;
-    let tsp_instance = TravellingSalesmanProblemInstance::parse_from_xml(&xml[..]);
+    let tsp_instance = TravellingSalesmanProblemInstance::parse_from_xml(&xml[..])?;
+    let lower_bound = one_tree_lower_bound(&tsp_instance.graph);
+    println!("1-tree lower bound: {}", lower_bound);
     Ok(())
 }
