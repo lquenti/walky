fb7643cc5f084bfc0e2ed37fda8c47c8ee86ffe6
JayCeM <johann.carl@online.de>
2023-05-17 21:10:58 +0200

    break the prim algo into smaller functions

--- src/mst.rs
+++ src/mst.rs
@@ -2,10 +2,24 @@
 
 use crate::parser::{Edge, Graph};
 
+use rayon::prelude::*;
+
 /// Prims algorithm for computing an MST of the given `graph`.
 /// See [`prim_with_excluded_node`] for more details.
 pub fn prim(graph: &Graph) -> Graph {
-    prim_with_excluded_node(graph, graph.num_vertices())
+    prim_with_excluded_node_multi_threaded(graph, graph.num_vertices())
+}
+
+/// #TODO
+/// currently only single threaded
+pub fn prim_with_excluded_node_multi_threaded(graph: &Graph, excluded_vertex: usize) -> Graph {
+    prim_with_excluded_node::<Vec<(Edge, bool)>>(graph, excluded_vertex)
+}
+
+/// #TODO
+/// improve asymptotic performance by using a priority queue
+pub fn prim_with_excluded_node_single_threaded(graph: &Graph, excluded_vertex: usize) -> Graph {
+    prim_with_excluded_node::<Vec<(Edge, bool)>>(graph, excluded_vertex)
 }
 
 /// greedy algorithm:
@@ -20,7 +34,7 @@ pub fn prim(graph: &Graph) -> Graph {
 /// todo: add source for the algorithm
 ///
 /// todo: make the implementation more pretty and more rust ideomatic
-pub fn prim_with_excluded_node(graph: &Graph, excluded_vertex: usize) -> Graph {
+pub fn prim_with_excluded_node<D: FindMinCostEdge>(graph: &Graph, excluded_vertex: usize) -> Graph {
     let num_vertices = graph.num_vertices();
     let unconnected_node = num_vertices;
 
@@ -32,69 +46,71 @@ pub fn prim_with_excluded_node(graph: &Graph, excluded_vertex: usize) -> Graph {
 
     // `dist_from_mst[i]` stores the edge with that the vertex i can be connected to the MST
     // with minimal cost.
-    let mut dist_from_mst: Vec<Edge> = vec![
+    let mut dist_from_mst = D::from_default_value(
         // base case: every vertex is "connected" to the unconnected node with cost f64::INFINITY
         Edge {
             cost: f64::INFINITY,
             to: unconnected_node,
-        };
-        num_vertices + 1
-    ];
+        },
+        num_vertices + 1,
+    );
 
     // Vertex at index unconnected_node is special: it is not connected to the rest of the graph,
     // and has distance INFINITY to every other node.
     // It is used as a base case.
 
-    // start with vertex 0
+    // start with vertex 0, or with vertex 1 if vertex 0 shall be excluded
     let start_index = if excluded_vertex != 0 { 0 } else { 1 };
-    dist_from_mst[start_index] = Edge {
-        to: start_index,
-        cost: 0.,
-    };
+    dist_from_mst.set_cost(
+        start_index,
+        Edge {
+            to: start_index,
+            cost: 0.,
+        },
+    );
+    dist_from_mst.set_excluded_vertex(excluded_vertex);
 
-    // at max. num_vertices many iterations, for every vertex one
+    // iterate over maximally `num_vertices` many iterations (for every vertex one)
     for _ in 0..=num_vertices {
-        let mut next_vertex = unconnected_node;
-        for i in 0..num_vertices {
-            // get the index of the vertex that is currently not in the MST
-            // and has minimal cost to connect to the mst
-            if !vertex_in_mst[i]
-                && dist_from_mst[next_vertex].cost > dist_from_mst[i].cost
-                && i != excluded_vertex
-            {
-                next_vertex = i;
-            }
-        }
+        //let mut next_vertex = unconnected_node;
+        //for i in 0..num_vertices {
+        //    // get the index of the vertex that is currently not in the MST
+        //    // and has minimal cost to connect to the mst
+        //    if !vertex_in_mst[i]
+        //        && dist_from_mst[next_vertex].cost > dist_from_mst[i].cost
+        //        && i != excluded_vertex
+        //    {
+        //        next_vertex = i;
+        //    }
+        //}
+        let (next_vertex, next_edge) = dist_from_mst.find_edge_with_minimal_cost(Edge {
+            to: num_vertices,
+            cost: f64::INFINITY,
+        });
 
-        // when we reach a unreachable vertex (like index num_vertices),
+        // when we reach an unreachable vertex (like index num_vertices),
         // we are finished
-        if dist_from_mst[next_vertex].cost == f64::INFINITY {
+        if next_edge.cost == f64::INFINITY {
             break;
         }
 
         // add next_vertex to the mst
-        vertex_in_mst[next_vertex] = true;
+        dist_from_mst.mark_vertex_as_used(next_vertex);
         if next_vertex != start_index {
-            let connecting_edge = dist_from_mst[next_vertex].clone();
+            //let connecting_edge = dist_from_mst[next_vertex].clone();
             let reverse_edge = Edge {
                 to: next_vertex,
-                cost: connecting_edge.cost,
+                cost: next_edge.cost,
             };
-            let connection_from = connecting_edge.to;
+            let connection_from = next_edge.to;
             let connection_to = next_vertex;
-            mst_adj_list[connection_to].push(connecting_edge);
+            mst_adj_list[connection_to].push(next_edge);
             mst_adj_list[connection_from].push(reverse_edge);
         }
 
         // update the minimal connection costs foll all newly adjacent vertices
         for edge in graph[next_vertex].iter() {
-            //dist_from_mst[edge.to] = f64::min(dist_from_mst[edge.to], edge.cost);
-            if edge.cost < dist_from_mst[edge.to].cost {
-                dist_from_mst[edge.to] = Edge {
-                    to: next_vertex,
-                    cost: edge.cost,
-                }
-            }
+            dist_from_mst.update_minimal_cost(next_vertex, *edge);
         }
     }
 
@@ -103,8 +119,79 @@ pub fn prim_with_excluded_node(graph: &Graph, excluded_vertex: usize) -> Graph {
     Graph::from(mst_adj_list)
 }
 
+/// This trait reflects a datastructure,
+/// that holds Edges and can give back the edge with minimal cost,
+/// as well as update the cost of edges.
+pub trait FindMinCostEdge {
+    fn from_default_value(default_val: Edge, size: usize) -> Self;
+    fn find_edge_with_minimal_cost(&self, base_case: Edge) -> (usize, Edge);
+    /// update the connection cost of `edge_to.to`.
+    /// If `edge_to.cost` is less than the current cost, the cost decreases to
+    /// `edge_to.cost` and `from` gets saved as the connecting vertex.
+    /// If it is higher, the cost does *not* increase.
+    /// If provided with the edge `from --> edge_to.to`,
+    /// the structure will then possibly remember the reverse edge `from <-- edge_to.to`
+    fn update_minimal_cost(&mut self, from: usize, edge_to: Edge);
+
+    /// sets the cost of connecting from `from` to `edge_to.to` to the value `edge_to.cost`.
+    fn set_cost(&mut self, from: usize, edge_to: Edge);
+
+    /// sets which vertex to exclude/ignore in the computations
+    fn set_excluded_vertex(&mut self, excluded_vertex: usize);
+
+    fn mark_vertex_as_used(&mut self, used_vertex: usize);
+}
+
+/// Edge: holds the (currently minimal) connection cost,
+/// and the vertex to which to connect to the MST
+///
+/// bool: true, if the Vertex is in the MST, false if the vertex is not in the MST.
+impl FindMinCostEdge for Vec<(Edge, bool)> {
+    fn from_default_value(default_val: Edge, size: usize) -> Self {
+        vec![(default_val, false); size]
+    }
+
+    fn find_edge_with_minimal_cost(&self, base_case: Edge) -> (usize, Edge) {
+        let mut next_vertex = base_case.to;
+        let mut reverse_edge = base_case;
+        for (i, &(edge, used_in_mst)) in self.iter().enumerate() {
+            // get the index of the vertex that is currently not in the MST
+            // and has minimal cost to connect to the mst
+            if !used_in_mst && reverse_edge.cost > edge.cost {
+                next_vertex = i;
+                reverse_edge = edge;
+            }
+        }
+        (next_vertex, reverse_edge)
+    }
+
+    fn update_minimal_cost(&mut self, from: usize, edge_to: Edge) {
+        //self[edge_to.to] = f64::min(self[edge_to.to], edge.cost);
+        if edge_to.cost < self[edge_to.to].0.cost {
+            self[edge_to.to].0 = Edge {
+                to: from,
+                cost: edge_to.cost,
+            };
+        }
+    }
+
+    fn set_cost(&mut self, from: usize, edge_to: Edge) {
+        self[from].0 = edge_to;
+    }
+
+    fn mark_vertex_as_used(&mut self, used_vertex: usize) {
+        self[used_vertex].1 = true;
+    }
+
+    fn set_excluded_vertex(&mut self, excluded_vertex: usize) {
+        self.mark_vertex_as_used(excluded_vertex);
+    }
+}
+
 #[cfg(test)]
 mod test {
+    use quickcheck_macros::quickcheck;
+
     use super::*;
 
     #[test]
@@ -245,6 +332,41 @@ mod test {
             vec![Edge { to: 1, cost: 0.1 }, Edge { to: 2, cost: 0.1 }],
         ]);
 
-        assert_eq!(expected, prim_with_excluded_node(&graph, 0));
+        assert_eq!(expected, prim_with_excluded_node_multi_threaded(&graph, 0));
+    }
+
+    #[test]
+    fn prim_single_and_multi_threaded_agree() {
+        let graph = Graph::from(vec![
+            //vertex 0
+            vec![
+                Edge { to: 1, cost: 1.0 },
+                Edge { to: 2, cost: 0.1 },
+                Edge { to: 3, cost: 2.0 },
+            ],
+            //vertex 1
+            vec![
+                Edge { to: 0, cost: 1.0 },
+                Edge { to: 2, cost: 5.0 },
+                Edge { to: 3, cost: 0.1 },
+            ],
+            //vertex 2
+            vec![
+                Edge { to: 0, cost: 0.1 },
+                Edge { to: 1, cost: 1.1 },
+                Edge { to: 3, cost: 0.1 },
+            ],
+            //vertex 3
+            vec![
+                Edge { to: 0, cost: 2.0 },
+                Edge { to: 1, cost: 0.1 },
+                Edge { to: 2, cost: 0.1 },
+            ],
+        ]);
+        let excluded_vertex = 0;
+        assert_eq!(
+            prim_with_excluded_node_single_threaded(&graph, excluded_vertex),
+            prim_with_excluded_node_multi_threaded(&graph, excluded_vertex)
+        );
     }
 }
