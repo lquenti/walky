bcdcb9f9f91593a9099cd693dc0f4265f3129411
JayCeM <johann.carl@online.de>
2023-06-11 14:58:48 +0200

    add 3 modes of computation

---
+++ src/computation_mode.rs
@@ -0,0 +1,13 @@
+//! This module contains 3 constants,
+//! used for algorithms to distinguish between sqeuential, parallel, or MPI-based computation.
+//! Ideally, these constants should belong into one Enum,
+//! but at the moment rust does not support const generics for Enums.
+
+/// use single-threaded computation
+pub const SEQ_COMPUTATION: usize = 0;
+
+/// use multi-threaded computation, on one machine
+pub const PAR_COMPUTATION: usize = 1;
+
+/// use multiple machinges to parallelize computation, via MPI
+pub const MPI_COMPUTATION: usize = 2;
