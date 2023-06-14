f271f05362c7078004685d9cd1b7bc7f2902d8b4
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-06-14 15:37:51 +0200

    benchmarking integrated into current walky

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -3,12 +3,15 @@
 # change how you want, its precomputed from 3/50
 # If you need more, see ../utils/benchmark_gen.py
 MIN_SIZE=3
-MAX_SIZE=5
+MAX_SIZE=9
+BINARY=$(readlink -f "../target/release/walky")
 
 pushd ..
 
 cargo build --release
-hyperfine --warmup 10 --runs 100 --parameter-scan N $MIN_SIZE $MAX_SIZE --export-json results.json "echo {N}"
+# Since hyperfine spawns a shell, we use the abs path everywhere
+# Add --show-output for debugging
+hyperfine --warmup 10 --runs 100 --parameter-scan N $MIN_SIZE $MAX_SIZE --export-json results.json "$BINARY $(readlink -f ./benchmarks/inputs)/{N}.xml"
 
 popd
 mv ../results.json .
