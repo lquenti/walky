47a7099823b442772d016fd52724d4c9d4129646
JayCeM <johann.carl@online.de>
2023-06-19 22:29:16 +0200

    add missing cfg(feature = "mpi" tag

--- src/solvers/approximate/matching.rs
+++ src/solvers/approximate/matching.rs
@@ -49,6 +49,7 @@ pub fn approx_min_cost_matching<const MODE: usize>(
     match MODE {
         SEQ_COMPUTATION => improve_matching(graph, matching.as_mut_slice(), tries),
         PAR_COMPUTATION => par_improve_matching(graph, matching.as_mut_slice(), tries),
+        #[cfg(feature = "mpi")]
         MPI_COMPUTATION => todo!(),
         _ => panic_on_invaid_mode::<MODE>(),
     }
