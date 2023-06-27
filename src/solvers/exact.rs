c3499b1ccbefedd58446b9d58b2f19cdded3708f
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-06-27 19:00:34 +0200

    untested, hacky MPI solution

--- src/solvers/exact.rs
+++ src/solvers/exact.rs
@@ -10,7 +10,10 @@ use crate::{
     mst,
 };
 
-use rustc_hash::{FxHashMap, FxHasher, FxHashSet};
+use rustc_hash::{FxHashMap, FxHashSet, FxHasher};
+
+//#[cfg(feature = "mpi")]
+use mpi::{collective::UserOperation, traits::*};
 
 /// Simplest possible solution: just go through all the nodes in order.
 /// No further optimizations. See [`next_permutation`] on how the permutations are generated.
@@ -624,9 +627,7 @@ fn get_next_value(current_digits: &mut [usize], basis: usize) -> Option<Vec<usiz
 /// If that fails we assume that parallelism is not available.
 
 pub fn threaded_solver(graph_matrix: &NAMatrix) -> Solution {
-    let res = threaded_solver_generic(graph_matrix, 3, None);
-    println!("end res {:?}", res);
-    res
+    threaded_solver_generic(graph_matrix, 3, None)
 }
 
 pub fn threaded_solver_generic(
@@ -663,17 +664,16 @@ pub fn threaded_solver_generic(
                 let mut current_prefix: Vec<usize> = Vec::new();
                 current_prefix.reserve(graph_matrix.dim());
 
-
                 // For each prefix
-                while let Some(next_value) = get_next_value(&mut min_digits, graph_matrix.dim())
-                {
+                while let Some(next_value) = get_next_value(&mut min_digits, graph_matrix.dim()) {
                     // early return if we have finished our chunk
                     if next_value == max_digits {
                         break;
                     }
 
                     // if it has at least two times the same digit, its not a valid path
-                    let is_unique = next_value.len() == next_value.iter().collect::<FxHashSet<_>>().len();
+                    let is_unique =
+                        next_value.len() == next_value.iter().collect::<FxHashSet<_>>().len();
                     if !is_unique {
                         continue;
                     }
@@ -687,7 +687,12 @@ pub fn threaded_solver_generic(
 
                     // use prefix
                     let mut result = (f64::INFINITY, Vec::new());
-                    _fifth_improved_solver_rec(graph_matrix, &mut current_prefix, starting_cost, &mut result);
+                    _fifth_improved_solver_rec(
+                        graph_matrix,
+                        &mut current_prefix,
+                        starting_cost,
+                        &mut result,
+                    );
 
                     // clear prefix
                     current_prefix.clear();
@@ -709,6 +714,244 @@ pub fn threaded_solver_generic(
         .unwrap()
         .into_inner()
         .unwrap()
+}
+
+//#[cfg(feature = "mpi")]
+pub fn mpi_solver(graph_matrix: &NAMatrix) -> Solution {
+    let res = mpi_solver_generic(graph_matrix, 3);
+    println!("end res {:?}", res);
+    res
+}
+
+// TODO merge with other
+#[derive(Default, Clone, Copy, Equivalence)]
+//#[cfg(feature = "mpi")]
+struct MPICostRank(f64, i32);
+
+const UPDATE_REQUEST_TAG: mpi::Tag = 0;
+
+
+// TODO comment
+//#[cfg(feature = "mpi")]
+pub fn mpi_solver_generic(graph_matrix: &NAMatrix, prefix_length: usize) -> Solution {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let size = world.size();
+    let rank = world.rank();
+    let root = world.process_at_rank(0);
+
+    // Will be used once the entire computation is done
+    // Has to be available to both root and non-root as we broadcast into it
+    let mut winner = MPICostRank(f64::INFINITY, 0);
+    let mut winner_path = Vec::new();
+
+    if rank == 0 {
+        let mut current_winner = MPICostRank(f64::INFINITY, 0);
+
+        // we keep track if any processes are not finished yet
+        // If they are finished, they have to tell us...
+        let mut number_of_finished_proceses = 0;
+
+        // As long as somebody is not yet done, we still listen
+        while number_of_finished_proceses < size -1 {
+            // We accept any message.
+            // Unfortunately, the type has to be known beforehand,
+            // which is why we have to work with some magic numbers...
+            // What we expect are two types of messages:
+            //
+            // 1. I have computed a new prefix. Here is my cost and my rank.
+            //    Please update your global minimum accordingly and tell me the result.
+            //    Then, we just get a normal MPICostRank with a normal cost.
+            //    Once we recieved such a message, we then update our local minimum
+            //    and tell them the result
+            //
+            // 2. I have no more prefixes to compute.
+            //    I am done and idling at the barrier.
+            //    If my buddies are done as well, please come to the barrier as well.
+            //    Then, we get a Cost with -1.0.
+            //    There is no need to answer to that.
+
+            // First, get any message
+            // Unfortunately, we have to provide the same type
+            let (msg, _) = world.any_process().receive::<MPICostRank>();
+
+            // Check which type of message we have
+            if msg.0 >= 0.0 {
+                // We have the first type, as this is a legit length
+
+                // So first, we update our local one
+                if msg.0 < current_winner.0 {
+                    current_winner.0 = msg.0;
+                    current_winner.1 = msg.1;
+                }
+
+                // Then, we respond to that with our version
+                world.process_at_rank(msg.1).send(&(current_winner.0));
+                // Now they know what the best known one is and can continue working...
+            } else {
+                // This node just tells us that it is done.
+                number_of_finished_proceses += 1;
+            }
+        }
+
+        // Since all processes told us that they are done (message type 2)
+        // we can now also join the barrier, breaking it...
+        world.barrier();
+
+        // Save who won
+        winner = current_winner;
+    } else {
+
+        // -1 because we subtract the root rank
+        let (min_digits, max_digits) = split_up_b_ary_number_into_n_chunks(
+            prefix_length,
+            graph_matrix.dim(),
+            (size as usize) -1,
+            (rank as usize) - 1,
+        );
+        let mut local_solution = (f64::INFINITY, Vec::new());
+        let mut global_best_cost = f64::INFINITY;
+
+        let mut min_digits = min_digits;
+        let mut current_prefix: Vec<usize> = Vec::new();
+        current_prefix.reserve(graph_matrix.dim());
+        while let Some(next_value) = get_next_value(&mut min_digits, graph_matrix.dim()) {
+            // early return if we have finished our chunk
+            if next_value == max_digits {
+                break;
+            }
+
+            // if it has at least two times the same digit, its not a valid path
+            let is_unique = next_value.len() == next_value.iter().collect::<FxHashSet<_>>().len();
+            if !is_unique {
+                continue;
+            }
+
+            // fill prefix
+            for i in &next_value {
+                current_prefix.push(*i);
+            }
+            // calculate cost of current prefix
+            let starting_cost = graph_matrix.evaluate_path(&current_prefix);
+
+            // use prefix
+            let mut result = (f64::INFINITY, Vec::new());
+            _mpi_improved_solver_rec(
+                graph_matrix,
+                &mut current_prefix,
+                starting_cost,
+                &mut result,
+                global_best_cost
+            );
+
+            // clear prefix
+            current_prefix.clear();
+
+            // Now we evaluate...
+            // 1. We update the local best
+            if result.0 < local_solution.0 {
+                local_solution.0 = result.0;
+                local_solution.1 = result.1.clone();
+            }
+            // 2. We request a new solution
+            // In order to reduce amount of RTTs, we do not ask whether we are better.
+            // Instead, we just send our local_solution, which _is_ the request to send us a
+            // response of the best cost
+            let sendbuf = MPICostRank(local_solution.0, rank);
+            root.send(&sendbuf);
+
+            // 3. We recieve the best global result
+            let (msg, _) = root.receive::<f64>();
+            global_best_cost = msg;
+
+            // We can now compute with the globally best cost in mind.
+        }
+
+        // Now that we have done all of our jobs, we wait for the other processes to complete
+        world.barrier();
+
+        // save the path
+        winner_path = local_solution.1.clone();
+    }
+
+    // After the barrier was broken we can broadcast the winner rank and cost
+    root.broadcast_into(&mut winner);
+
+    // Now the winner can broadcast to everyone the path
+    world.process_at_rank(winner.1).broadcast_into(&mut winner_path);
+
+    // After we all know the cost and path, we can finally return with the exact result
+    (winner.0, winner_path)
+}
+
+fn _mpi_improved_solver_rec(
+    graph_matrix: &NAMatrix,
+    current_prefix: &mut Path,
+    current_cost: f64,
+    result: &mut Solution,
+    global_best_cost: f64
+) {
+    let n = graph_matrix.dim();
+    let mut current_cost = current_cost;
+
+    // Base case: Is this one better?
+    if current_prefix.len() == n {
+        // Add the last edge, finishing the circle
+        current_cost += graph_matrix.get(
+            *current_prefix.last().unwrap(),
+            *current_prefix.first().unwrap(),
+        );
+
+        let best_cost = result.0;
+        if current_cost < best_cost {
+            result.0 = current_cost;
+            result.1 = current_prefix.clone();
+        }
+        return;
+    }
+
+    // Branch down with branching factor n-k, where k is the length of current_prefix
+    for i in 0..n {
+        // We do not visit twice
+        if current_prefix.contains(&i) {
+            continue;
+        }
+
+        current_prefix.push(i);
+        // If this is a single element, we do not have an edge yet
+        if current_prefix.len() == 1 {
+            _mpi_improved_solver_rec(graph_matrix, current_prefix, current_cost, result, global_best_cost);
+            current_prefix.pop();
+            continue;
+        }
+
+        // Calculate the cost of our new edge
+        let from = current_prefix.len() - 2;
+        let to = from + 1;
+        let cost_last_edge = graph_matrix.get(current_prefix[from], current_prefix[to]);
+        current_cost += cost_last_edge;
+
+        // If our current sub-tour, together with a lower bound, is already bigger than the whole
+        // tour the whole tour will definitely be bigger than our previous best version
+        let lower_bound_graph =
+            mst::prim_with_excluded_node_single_threaded(graph_matrix, current_prefix);
+        let lower_bound = lower_bound_graph.directed_edge_weight();
+
+        // TODO comment me
+        let best_known_solution = if global_best_cost < result.0 {
+            global_best_cost
+        } else {
+            result.0
+        };
+
+        if current_cost + lower_bound <= best_known_solution {
+            _mpi_improved_solver_rec(graph_matrix, current_prefix, current_cost, result, global_best_cost);
+        }
+
+        // Remove the last edge
+        current_cost -= cost_last_edge;
+        current_prefix.pop();
+    }
 }
 
 #[cfg(test)]
@@ -1277,7 +1520,7 @@ mod exact_solver {
             fourth_improved_solver,
             fifth_improved_solver,
             sixth_improved_solver,
-            threaded_solver
+            threaded_solver,
         ]
         .iter()
         {
@@ -1322,7 +1565,7 @@ mod exact_solver {
             fourth_improved_solver,
             fifth_improved_solver,
             sixth_improved_solver,
-            threaded_solver
+            threaded_solver,
         ]
         .iter()
         {
@@ -1367,7 +1610,7 @@ mod exact_solver {
             fourth_improved_solver,
             fifth_improved_solver,
             sixth_improved_solver,
-            threaded_solver
+            threaded_solver,
         ]
         .iter()
         {
