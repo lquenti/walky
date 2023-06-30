804d5839714af4b99ef5649694d15a1aca4d319c
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-06-30 13:28:36 +0200

    reduce the range we gen

--- utils/gen_matrix_fast/gen.sh
+++ utils/gen_matrix_fast/gen.sh
@@ -1,9 +1,9 @@
 cargo build --release
 rm -rf results
 mkdir results
-for ((i=3; i<=1007; i+=7))
+for ((i=3; i<=207; i+=7))
 do
-  echo "$i/1007"
+  echo "$i/207"
   ./target/release/gen_matrix_fast $i > ./results/$i.xml &
   ./target/release/gen_matrix_fast $((i+1)) > ./results/$((i+1)).xml &
   ./target/release/gen_matrix_fast $((i+2)) > ./results/$((i+2)).xml &
