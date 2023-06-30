750e17fb1df87baad58dcbdc77e405a03b1dc5c8
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-06-30 16:08:50 +0200

    1-tree STMT benchmarks

---
+++ benchmarks/1-tree/STMT/plot_results.py
@@ -0,0 +1,33 @@
+import json
+import matplotlib.pyplot as plt
+
+BASE_FOLDER = "results"
+MIN_SIZE = 100
+MAX_SIZE = 3000
+STEP = 100
+
+single_threaded = ([], [])
+multi_threaded = ([], [])
+for size in range(MIN_SIZE, MAX_SIZE+1, STEP):
+    # single threaded
+    with open(f"./{BASE_FOLDER}/result_{size}_single-threaded.json") as fp:
+        obj = json.load(fp)
+    single_threaded[0].append(size)
+    single_threaded[1].append(obj["median"]) # could also use mean
+
+    # multi threaded
+    with open(f"./{BASE_FOLDER}/result_{size}_multi-threaded.json") as fp:
+        obj = json.load(fp)
+    multi_threaded[0].append(size)
+    multi_threaded[1].append(obj["median"]) # could also use mean
+
+plt.plot(*single_threaded, label = "single threaded")
+plt.plot(*multi_threaded, label = "multi threaded")
+
+#plt.ylim(y_min, y_max)
+plt.xlabel('Graph Size')
+plt.ylabel('time in seconds')
+plt.title('Exact Solver Single Node')
+plt.legend()
+plt.show()
+
---
+++ benchmarks/1-tree/STMT/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# cluster conf
+#PROGRAM="/home/uni11/gwdg1/GWDG/lars.quentin01/code/playground/walky/target/release/walky"
+#XML_PATH="/home/uni11/gwdg1/GWDG/lars.quentin01/code/playground/walky/utils/gen_matrix_fast/results"
+
+# local testing
+PROGRAM="/home/lquenti/code/walky/target/release/walky"
+XML_PATH="/home/lquenti/code/walky/utils/gen_matrix_fast/results/"
+
+rm -rf results
+mkdir results
+
+for ((N=100; N<=3000; N+=100)); do
+  echo "${N}/3000"
+  for PARALLELISM in "single-threaded" "multi-threaded"; do
+    command="python3 ../../run_often.py \"$PROGRAM lower-bound one-tree -p $PARALLELISM ${XML_PATH}/$N.xml\" ./results/result_${N}_${PARALLELISM}.json"
+    eval "$command"
+  done
+done
+
