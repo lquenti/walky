fbdef6f96083b1ca40cb14edd4e356cc6783973b
Johann Carl Meyer <johann.carl@online.de>
2023-10-05 11:16:47 +0200

    remove accidental insertion

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::error::Error;
 use clap::Parser;
 use walky::cli::Cli;
 
-#![warn(missing_docs)]
-
 /// parse the command line arguments and pass them to `[walky::run]`
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
